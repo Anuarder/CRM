b0b1e066bf73f672bbf8423dae8eda2b0a22f55c
Anuarder <anuar.ibraev97@gmail.com>
2019-04-26 18:55:48 +0600

    add container class

--- template/src/main/App.js
+++ template/src/main/App.js
@@ -8,8 +8,10 @@ import Header from '../components/Header/Header'
 function App() {
     return (
         <div className="App">
-            <div className="header-component">
-                <Header></Header>
+            <div className="container">
+                <div className="header-component">
+                    <Header></Header>
+                </div>
             </div>
         </div>
     );
